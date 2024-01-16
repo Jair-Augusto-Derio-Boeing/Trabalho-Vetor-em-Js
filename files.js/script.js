637a46fdf57b1335d34d396cff34f54a16b09301
MASTERDOSNOOBS <jairderioboeing@gmail.com>
2024-01-16 00:00:28 -0300

    up .js

--- files.js/script.js
+++ files.js/script.js
@@ -1,49 +1,57 @@
-    function Main(){
-        console.log("a");
-        switch (Menu()) {
-            case 1:
-                console.log("ass");
-                break;
-            
-            case 2:
+function Main() {
+    switch (Menu()) {
+        case 1:
+            ColetaVetor();
+            break;
 
-                break;
-            
-            case 3:
+        case 2:
 
-                break;
+            break;
 
+        case 3:
 
-            case 4:
-
-             break;
+            break;
 
 
-            case 0:
+        case 4:
 
-                break;
+            break;
 
-            default:
-                console.log("Opção Inválida!");
-                break;
-        }
+
+        case 0:
+
+            break;
+
+        default:
+            console.log("Opção Inválida!");
+            break;
     }
+}
 
 
-    
-     function Menu() {
-            
-       
-        console.log("      MENU DE OPÇÕES");  
-        console.log(" ");
-        console.log("1ª opção: Coleta Dados do Vetor");
-        console.log("2ª opção: Gera valores randômicos para o Vetor.");
-        console.log("3ª opção: Imprime dados do Vetor.");
-        console.log("4ª opção: Imprime Vetor Invertido. ");
-        console.log(" ");
-        console.log("Digite 0 para sair.");
-        console.log(" ");
-        let escolha = +prompt("Digite a opção desejada:");
-        return escolha;
 
+function Menu() {
+
+
+    console.log("      MENU DE OPÇÕES");
+    console.log(" ");
+    console.log("1ª opção: Coleta Dados do Vetor");
+    console.log("2ª opção: Gera valores randômicos para o Vetor.");
+    console.log("3ª opção: Imprime dados do Vetor.");
+    console.log("4ª opção: Imprime Vetor Invertido. ");
+    console.log(" ");
+    console.log("Digite 0 para sair.");
+    console.log(" ");
+    let escolha = +prompt("Digite a opção desejada:");
+    return escolha;
+
+}
+
+function ColetaVetor() {
+    const array = []
+    const size = 10
+    for (let i = 0; i < size; i++) {
+        array[i] = +prompt(`Informe o ${i + 1}º Valor do Vetor`)
+        console.log(array[i]);
     }
+}
